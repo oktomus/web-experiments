7584da3bbd0e0b90580a20799307c4252b0dfbbd
Kevin Masson <hi@oktomus.com>
2020-02-08 12:39:47 +0100

    Use threejs module for my tests

--- threejs/getting-started/app.js
+++ threejs/getting-started/app.js
@@ -1,3 +1,5 @@
+import * as THREE from './../../js/threejs/build/three.module.js';
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
